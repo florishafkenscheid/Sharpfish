67f53891e097577a75115d1b0ae334abc71fc77b
florishafkenscheid <floris@hafkenscheid.com>
2025-03-12 14:05:57 +0100

    feat: ResponseParser class implementation

--- ResponseParser.cs
+++ ResponseParser.cs
@@ -8,5 +8,34 @@ namespace Sharpfish
 {
     internal class ResponseParser
     {
+        private const string INFO_LINE = "info";
+        private const string BESTMOVE_LINE = "bestmove";
+        private const string READYOK = "readyok";
+        private Dictionary<string, Action<string>> _parsers;
+
+        public ResponseParser() 
+        {
+            _parsers = new Dictionary<string, Action<string>>();
+        }
+
+        public ChessMove ParseBestMove(string response)
+        {
+            return null;
+        }
+
+        public MoveAnalysis ParseMoveAnalysis(string response)
+        {
+            return null;
+        }
+
+        public EngineOptions ParseEngineOptions(string response)
+        {
+            return null;
+        }
+
+        public bool ParseReadyOk(string response)
+        {
+            return true;
+        }
     }
 }
