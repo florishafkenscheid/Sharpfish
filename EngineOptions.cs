938bc2b5cf428daf920bf58e3f0091a276b42013
florishafkenscheid <floris@hafkenscheid.com>
2025-03-12 13:10:07 +0100

    feat: EngineOptions constructor and attributes

--- EngineOptions.cs
+++ EngineOptions.cs
@@ -8,5 +8,32 @@ namespace Sharpfish
 {
     internal class EngineOptions
     {
+        private Dictionary<string, string> _options { get; set; }
+        private int _skillLevel { get; set; }
+        private int _threads { get; set; }
+        private int _hashSize { get; set; }
+        private int _multiPV { get; set; }
+        private bool _useNNUE { get; set; }
+        private StockfishEngine _engine { get; set; }
+
+
+        public EngineOptions(StockfishEngine engine)
+        {
+            _engine = engine;
+
+            // Set defaults
+            _options = new Dictionary<string, string>()
+                {
+                    { "Threads", "4" }, // Relatively low entry barrier, don't want to mess with detecting CPU
+                    { "Hash", "256"}, // In MBs
+                    { "MultiPV", "1"}, // Show only the best line
+                    { "Skill Level", "20"}, // 0-20, default at 20
+                };
+
+            // Create command to send defaults to engine
+            CommandBuilder commandBuilder = new CommandBuilder();
+            string command = string.Empty; // TODO
+            engine.SendCommand(command);
+        }
     }
 }
--- StockfishEngine.cs
+++ StockfishEngine.cs
@@ -18,59 +18,72 @@ namespace Sharpfish
         private bool _isAnalyzing { get; set; }
 
         private EngineOptions _engineOptions { get; set; }
-        private CommandBuilder _commandBuilder { get; set;  }
-        private ResponseParser _responseParser { get; set;  }
+        private CommandBuilder _commandBuilder { get; set; }
+        private ResponseParser _responseParser { get; set; }
         private ChessPosition _curentPosition { get; set; }
-
-        private object _lockObject;
 
         public EventHandler<MoveAnalysis> AnalysisUpdated;
 
         public StockfishEngine(string path)
         {
             _enginePath = path;
-            
-            _engineProcess = Process.Start(path);
-            _engineOptions = new EngineOptions();
+
+            _engineProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = path,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            _engineProcess.Start();
+            _engineInput = _engineProcess.StandardInput;
+            _engineOutput = _engineProcess.StandardOutput;
+
+            _engineOptions = new EngineOptions(this);
             _commandBuilder = new CommandBuilder();
             _responseParser = new ResponseParser();
-
-
+            _curentPosition = new ChessPosition();
+            AnalysisUpdated = delegate { };
         }
 
         public void SendCommand(string command)
         {
-
+            _engineInput.WriteLine(command);
+            _engineInput.Flush();
         }
 
         public bool IsReady()
         {
-
+            return true;
         }
 
         public void SetPosition(ChessPosition position)
         {
-
+            _curentPosition = position;
         }
 
         public void StartAnalysis(int depth)
         {
-
+            
         }
 
         public void StopAnalysis()
         {
-
+            
         }
 
         public ChessMove GetBestMove(int timeInMs)
         {
-
+            return null;
         }
 
         public void SetOption(string name, string value)
         {
-
+            
         }
     }
 }
