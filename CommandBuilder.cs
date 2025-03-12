e96b8727fc466a41a352a38eea83b3b78f3cb54f
florishafkenscheid <floris@hafkenscheid.com>
2025-03-12 14:17:02 +0100

    feat: CommandBuilder class implementation

--- CommandBuilder.cs
+++ CommandBuilder.cs
@@ -8,5 +8,21 @@ namespace Sharpfish
 {
     internal class CommandBuilder
     {
+        private const string UCI_NEW_GAME = "ucinewgame";
+        private const string POSITION = "position";
+        private const string GO = "go";
+        private const string STOP = "stop";
+        private const string SET_OPTION = "setoption name";
+        private const string IS_READY = "isready";
+        private const string QUIT = "quit";
+
+        public CommandBuilder() { }
+
+        public string BuildPositionCommand(ChessPosition position) { }
+
+        public string BuildGoCommand(int depth, int timeMs) { }
+        public string BuildSetOptionCommand(string name, string value) { }
+        public string BuildUciNewGameCommand() { }
+        public string BuildStopCommand() { }
     }
 }
