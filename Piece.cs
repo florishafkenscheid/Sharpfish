b5101258e0711965b75540d0f7cc8d99edfb9db9
florishafkenscheid <floris@hafkenscheid.com>
2025-03-13 14:57:17 +0100

    feat: Piece class implementation

--- Piece.cs
+++ Piece.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Sharpfish
 {
-    internal class Piece
+    public abstract class Piece
     {
+        private string _position;
+        private readonly bool _isWhite;
+
+        protected Piece(string type, string position, bool isWhite)
+        {
+            _position = position;
+            _isWhite = isWhite;
+        }
+
+        public string Position
+        {
+            get => _position;
+            set => _position = value;
+        }
+
+        public bool IsWhite => _isWhite;
+
+        public abstract string Type { get; }
+        public abstract char Symbol { get; }
     }
 }
